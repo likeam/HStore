10e32016841b4a44d96ee80d7bdb997f2fa379d3
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-07-13 12:19:16 +0500

    Building Order api in frontend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,31 @@
 import express from "express";
 
-import { createOrder } from "../controllers/orderController.js";
-import { authenticate } from "../middlewares/authMiddleware.js";
+import {
+  createOrder,
+  getAllOrders,
+  getUserOrders,
+  countTotalOrders,
+  calculateTotalSales,
+  calcualteTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
+} from "../controllers/orderController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(authenticate, createOrder);
-
+router
+  .route("/")
+  .post(authenticate, createOrder)
+  .get(authenticate, authorizeAdmin, getAllOrders);
+router.route("/mine").get(authenticate, getUserOrders);
+router.route("/total-orders").get(countTotalOrders);
+router.route("/total-sales").get(calculateTotalSales);
+router.route("/total-sales-by-date").get(calcualteTotalSalesByDate);
+router.route("/:id").get(authenticate, findOrderById);
+router.route("/:id/pay").put(authenticate, markOrderAsPaid);
+router
+  .route("/:id/deliver")
+  .put(authenticate, authorizeAdmin, markOrderAsDelivered);
 export default router;
--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -3,3 +3,4 @@ export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCTS_URL = "/api/products";
 export const UPLOADS_URL = "/api/uploads";
+export const ORDERS_URL = "/api/orders";
