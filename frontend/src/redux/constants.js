e84c65fd9e41c69a051d04997edc48008ff52d5c
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-07-31 22:43:32 +0500

    Removing Error

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "http://localhost:3000";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCTS_URL = "/api/products";
